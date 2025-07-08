dbf65e0d944f95ad72d78741bcb88f69fd7bd655
Muwa <Muwa@DESKTOP-S0U5V3J>
2025-07-08 15:16:24 +0300

    cellpose API added

---
+++ cellposeapi.py
@@ -0,0 +1,14 @@
+from gradio_client import Client, file, handle_file
+import os
+outputDir = r"D:\Projects\VICTORIA"
+
+client = Client("mouseland/cellpose")
+result = client.predict(
+        filepath=[handle_file(os.path.join(outputDir, "15-SE-1k-T1.bmp"))],
+        resize=1000,
+        max_iter=250,
+        flow_threshold=0.4,
+        cellprob_threshold=0,
+        api_name="/cellpose_segment"
+    )
+print(result)
